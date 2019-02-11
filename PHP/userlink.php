cac8487acec0f341e963d77219174d93a172c355
mostafahussien123 <mostafa-hussien2010@hotmail.com>
2019-02-11 20:45:30 +0200

    Links
    msh last

--- PHP/userlink.php
+++ PHP/userlink.php
@@ -9,8 +9,39 @@ class UserLinks
 
   function __construct(argument)
   {
+$sql ="SELECT * FROM userlink WHERE id='$id'";
+    $result = mysqli_query($GLOBALS["db"], $sql);
 
-  }
+    if(mysqli_num_rows($result) == 1){
+
+      while($row = mysqli_fetch_array($result)){
+        $this->ID = $row["id"];
+        $this->typeid = $row["typeID"];
+        $this->linkid = $row["LinkID"];
+       
+
+ function addlink ($obj){
+
+   $sql="INSERT INTO `userlink` (`id`, `typeID`, `LinkID`) VALUES ( '$obj->typeid', '$obj->linkid')";
+   mysql_query($sql);
+ }
+
+
+function deletelink ($obj){
+
+$sql="DELETE FROM `userlink` WHERE `userlink`.`ID` = '$obj->ID'";
+        mysql_query($sql);
+
+}
+
+function updatelink (){
+
+ $sql="UPDATE `userlink` SET `typeID` ='$typeid', `LinkID` ='$linkid' ";
+  mysql_query($sql);
+
+}
+
+
 }
 
 

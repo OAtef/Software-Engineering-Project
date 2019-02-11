1352767351e19b51d29c2a5f20a26a0e9964e8a1
Seif <seifelmosalamy@gmail.com>
2019-02-11 21:30:32 +0200

    latest seif

--- PHP/type.php
+++ PHP/type.php
@@ -1,4 +1,5 @@
 <?php
+include_once('dbConnection.php');
 
 class Types
 {
@@ -6,11 +7,53 @@ class Types
   public $ID;
   public $typename;
 
-  function __construct(argument)
-  {
+   function __construct($id){
+
+      $sql ="SELECT * FROM users WHERE id='$id'";
+      $result = mysqli_query($GLOBALS["db"], $sql);
+
+      if(mysqli_num_rows($result) == 1){
+
+        while($row = mysqli_fetch_array($result)){
+          $this->ID = $row["id"];
+          $this->typename = $row["typeName"];
+        
+        }
+
+      }
+    }
+  static function addusertype($obj){
+
+    $add_user_query="INSERT INTO usertype (id, typeName)  VALUES ('$obj->$ID', '$obj->$typename')";
+      mysqli_query($GLOBALS["db"], $add_user_query);
 
   }
-}
 
+  static function deleteusertype($obj){
 
+      $delete_user_query = "DELETE FROM usertype WHERE usertype.id = $obj->$ID";
+      mysqli_query($GLOBALS["db"], $delete_user_query);
+      if (mysqli_affected_rows($GLOBALS["db"]) == 1) {
+        return 1;
+      }
+      else{
+        return 0;
+      }
+
+  }
+
+  static function updateusertype($obj){
+
+        $update_user_query = "UPDATE usertype SET id='$obj->$ID', typeName='$obj->$typename'";
+      mysqli_query($GLOBALS["db"], $update_user_query) or die(mysqli_error($mysqli));
+      if (mysqli_affected_rows($GLOBALS["db"]) == 1) {
+            return 1;
+          }
+          else{
+            return 0;
+          }
+
+  }
+  
+  }
 ?>

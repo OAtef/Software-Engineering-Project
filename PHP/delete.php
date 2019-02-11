39827a6f9f998ed99ea08410cd806c57152de064
HabibaHegazy <HabibaHegazy@users.noreply.github.com>
2019-02-11 18:38:13 +0200

    h

---
+++ PHP/delete.php
@@ -0,0 +1,27 @@
+<?php
+// include database connection
+include_once('users.php');
+ 
+try {
+     
+    // get record ID
+    // isset() is a PHP function used to verify if a value is there or not
+    $id=isset($_GET['id']) ? $_GET['id'] : die('ERROR: Record ID not found.');
+ 
+   $obj = new Users(NULL);
+   $return = $obj::deleteUser($id);
+     
+    if($return == 1){
+        // redirect to read records page and 
+        // tell the user record was deleted
+        header('Location: index.php?action=deleted');
+    }else{
+        die('Unable to delete record.');
+    }
+}
+ 
+// show error
+catch(PDOException $exception){
+    die('ERROR: ' . $exception->getMessage());
+}
+?>
